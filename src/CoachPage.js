b6622647f77db4f6414c3346f96e2025df935d76
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2025-08-06 22:21:08 -0400

    Update

--- src/CoachPage.js
+++ src/CoachPage.js
@@ -210,15 +210,20 @@ const CoachPage = ({ rawData }) => {
         )}
       </div>
 
-      <div style={{
-              marginTop: '1rem',
-              border: '1px solid #ccc',
-              borderRadius: '10px',
-              padding: '1rem',
-              background: '#f9f9f9',
-              fontSize: '0.9rem',
-              color: '#333'
-            }}>
+      <div   
+        style={{
+          marginTop: '1rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '100%',
+          maxWidth: '800px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          padding: '1rem',
+          background: '#f9f9f9',
+          fontSize: '0.9rem',
+          color: '#333',
+        }}>
               <h3 style={{ marginTop: 0, textAlign: 'center' }}>Radar Chart Key</h3>
               <ul style={{ listStyle: 'none', paddingLeft: 0, lineHeight: 1.5 }}>
                 <li><strong>Prob of NBA for 1st-Yrs</strong>: Percentile rank of average NBA chance for freshmen. Higher percentile means higher likelihood of making NBA.</li>
